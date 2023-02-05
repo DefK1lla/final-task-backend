76c7c519d6e168185e260c88df396d1ff3fbf18f
Sedrak Adoyan <set.adoyan@gmail.com>
2023-02-05 17:39:30 +0400

    refactor: add authController

---
+++ src/controllers/authController.ts
@@ -0,0 +1,70 @@
+import type { Request, Response } from 'express';
+
+import bcrypt from 'bcryptjs';
+
+import type { IDBUser } from '../types/User';
+
+import { CLIENT_URL, PASSWORD_SALT } from '../utils/config';
+
+import User from '../models/User';
+
+class AuthController {
+  googleCallback = (req: Request, res: Response) => {
+    res.redirect(CLIENT_URL as string);
+  };
+
+  localRegister = async (req: Request, res: Response) => {
+    try {
+      const { username, password } = req?.body;
+
+      if (
+        !username ||
+        !password ||
+        typeof username !== 'string' ||
+        typeof password !== 'string'
+      ) {
+        res.status(400).json({ error: 'Improper Values' });
+        return;
+      }
+
+      const user = (await User.findOne({
+        username,
+      }).lean()) as IDBUser;
+
+      if (user)
+        res.status(400).json({ error: 'User Already Exists' });
+
+      if (!user) {
+        const hashedPassword = await bcrypt.hash(
+          password,
+          PASSWORD_SALT
+        );
+        const newUser = new User({
+          username,
+          password: hashedPassword,
+        });
+        await newUser.save();
+        res
+          .status(200)
+          .json({ id: newUser._id, username: newUser.username });
+      }
+    } catch (e) {
+      console.log(e);
+      res.status(500).json({ message: 'Server Error' });
+    }
+  };
+
+  localLogin = (req: Request, res: Response) => {
+    const user = req.user as IDBUser;
+    res.status(200).json({ id: user._id, username: user.username });
+  };
+
+  logout = (req: Request, res: Response) => {
+    req.logOut(() => {
+      res.clearCookie('connect.sid');
+      res.status(200).json({ message: 'Logout' });
+    });
+  };
+}
+
+export default new AuthController();
--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,13 +1,8 @@
 import { Router } from 'express';
-import bcrypt from 'bcryptjs';
-
-import type { IDBUser } from '../types/User';
-
-import { CLIENT_URL, PASSWORD_SALT } from '../utils/config';
 
 import passport from '../libs/passport';
 
-import User from '../models/User';
+import authController from '../controllers/authController';
 
 const router = Router();
 
@@ -15,65 +10,20 @@ router.get(
   '/google',
   passport.authenticate('google', { scope: ['profile'] })
 );
-
 router.get(
   '/google/callback',
   passport.authenticate('google', {
     session: true,
   }),
-  (req, res) => {
-    res.redirect(CLIENT_URL as string);
-  }
+  authController.googleCallback
+);
+router.post('/register', authController.localRegister);
+router.post(
+  '/login',
+  passport.authenticate('local'),
+  authController.localLogin
 );
 
-router.post('/register', async (req, res) => {
-  try {
-    const { username, password } = req?.body;
-
-    if (
-      !username ||
-      !password ||
-      typeof username !== 'string' ||
-      typeof password !== 'string'
-    ) {
-      res.status(400).json({ error: 'Improper Values' });
-      return;
-    }
-
-    const user = (await User.findOne({ username }).lean()) as IDBUser;
-
-    if (user) res.status(400).json({ error: 'User Already Exists' });
-
-    if (!user) {
-      const hashedPassword = await bcrypt.hash(
-        password,
-        PASSWORD_SALT
-      );
-      const newUser = new User({
-        username,
-        password: hashedPassword,
-      });
-      await newUser.save();
-      res
-        .status(200)
-        .json({ id: newUser._id, username: newUser.username });
-    }
-  } catch (e) {
-    console.log(e);
-    res.status(500).json({ message: 'server error' });
-  }
-});
-
-router.post('/login', passport.authenticate('local'), (req, res) => {
-  const user = req.user as IDBUser;
-  res.status(200).json({ id: user._id, username: user.username });
-});
-
-router.get('/logout', (req, res) => {
-  req.logOut(() => {
-    res.clearCookie('connect.sid');
-    res.status(200).json({ message: 'logut' });
-  });
-});
+router.get('/logout', authController.logout);
 
 export default router;
