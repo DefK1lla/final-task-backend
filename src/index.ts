f245aa378000e4b4cafc8a08160595808e17d3b4
Sedrak Adoyan <set.adoyan@gmail.com>
2023-02-14 00:09:20 +0400

    fix: add httpOnly

--- src/index.ts
+++ src/index.ts
@@ -35,7 +35,7 @@ app.use(
     saveUninitialized: true,
     store: MongoStore.create({ mongoUrl: MONGODB_URI }),
     cookie: {
-      domain: 'localhost',
+      httpOnly: true,
       sameSite: 'none',
       secure: true,
       maxAge: 1000 * 60 * 60 * 24 * 7,
