9e36100e44a67222fb5152807e05b46c1d841778
Sedrak Adoyan <set.adoyan@gmail.com>
2023-02-14 00:56:20 +0400

    feat: add proxy

--- src/libs/passport.ts
+++ src/libs/passport.ts
@@ -39,6 +39,7 @@ passport.use(
       clientID: `${GOOGLE_CLIENT_ID}`,
       clientSecret: `${GOOGLE_CLIENT_SECRET}`,
       callbackURL: '/auth/google/callback',
+      proxy: true,
     },
     async (_, __, profile, cb) => {
       try {
