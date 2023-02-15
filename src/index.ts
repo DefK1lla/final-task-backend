e2ad0f4ef059e0df889607b46e66087345ec4c85
Sedrak Adoyan <set.adoyan@gmail.com>
2023-02-15 16:20:46 +0400

    fix: add secure

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,42 @@
-import express, { NextFunction, Request, Response } from "express";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import session from "express-session";
-import MongoStore from "connect-mongo";
+import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
+import MongoStore from 'connect-mongo';
 
-import { PORT, MONGODB_URI, CLIENT_URL, SECRET } from "./utils/config";
+import {
+  PORT,
+  MONGODB_URI,
+  CLIENT_URL,
+  SECRET,
+} from './utils/config';
 
-import passport from "./libs/passport";
-import router from "./routes";
-import errorHandler from "./utils/errorHandler";
+import passport from './libs/passport';
+import router from './routes';
+import errorHandler from './utils/errorHandler';
 
 const app = express();
 
 app.use(
   cors({
-    origin: [`${CLIENT_URL}`, "https://localhost:3000"],
+    origin: [`${CLIENT_URL}`, 'https://localhost:3000'],
     credentials: true,
   })
 );
 app.use(express.json());
+
+app.set('trust proxy', 1);
 app.use(
   session({
     secret: `${SECRET}`,
     resave: true,
     saveUninitialized: true,
     store: MongoStore.create({ mongoUrl: MONGODB_URI }),
-    cookie: { maxAge: 1000 * 60 * 60 * 24 * 7 },
+    cookie: {
+      sameSite: 'none',
+      secure: true,
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    },
   })
 );
 app.use(cookieParser());
